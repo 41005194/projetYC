2f0e1e39923eea65ed6c509882dc0e5a05f35182
Liam PROROVNER <41005194@parisnanterre.fr>
2025-11-03 17:12:48 +0100

    indications

--- devinette.c
+++ devinette.c
@@ -4,10 +4,12 @@ int main() {
 	int nb_secret = 32;
 	int nb_user;
 	scanf("%d", &nb_user);
-	if (nb_user == nb_secret) {
-		printf("Succès");
+	if (nb_user > nb_secret) {
+		printf("Trop grand !");
+	} else if (nb_user < nb_secret) {
+		printf("Trop petit !");
 	} else {
-		printf("Echec");
+		printf("Succès !");
 	}
 	return 0;
 }
