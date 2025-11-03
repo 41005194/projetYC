d165b9691ba3547d858b51c80ce836f83adab82c
Liam PROROVNER <41005194@parisnanterre.fr>
2025-11-03 16:54:17 +0100

    devinette + celebration

--- devinette.c
+++ devinette.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    printf("Hello World\n");
+    int number = rand() % 100 + 1; // Génère un nombre aléatoire entre 1 et 100
+    int guess = 0;
+
+    printf("Bienvenue au jeu de la devinette !\n");
+    printf("J'ai choisi un nombre entre 1 et 100. Pouvez-vous le deviner ?\n");
+
+        printf("Entrez votre devinette : ");
+        scanf("%d", &guess);
+
+        if (guess != number) {
+            printf("C'est raté !\n");
+        } else {
+            printf("Félicitations ! Vous avez deviné le nombre.\n");
+        }
+
     return 0;
 }
